ed7d51c1a8a0fa6447c794d45cfced6aefc0dfa0
JosLucero <109862350+JosLucero@users.noreply.github.com>
2024-08-01 18:41:11 -0600

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,1 +1,13 @@
-
+ using Microsoft.AspNetCore.Components.Web;                                                                              
+ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;                                                              
+ using Microsoft.Extensions.Configuration;                                                                               
+ using Microsoft.Extensions.DependencyInjection;                                                                         
+                                                                                                                         
+ var builder = WebAssemblyHostBuilder.CreateDefault(args);                                                               
+                                                                                                                         
+ builder.Services.AddControllers();                                                                                      
+ builder.Services.AddTransient<TemperatureConverter>();                                                                  
+                                                                                                                         
+ var host = builder.Build();                                                                                             
+                                                                                                                         
+ host.Run();
